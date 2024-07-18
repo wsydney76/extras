a720e95583d9987ca7caff434c333cf7bc5f3710
wsydn <wsydney@web.de>
2024-07-18 10:18:49 +0200

    Helper functions for Json columns

--- src/helpers/ExtrasHelper.php
+++ src/helpers/ExtrasHelper.php
@@ -4,31 +4,75 @@ namespace wsydney76\extras\helpers;
 
 use Craft;
 use craft\base\Component;
-use craft\errors\EntryTypeNotFoundException;
-use craft\errors\InvalidFieldException;
+use craft\base\FieldLayoutProviderInterface;
+use yii\base\InvalidArgumentException;
 
 class ExtrasHelper extends Component
 {
-    public static function getValueSql(string $entryTypeHandle, string $fieldHandle): string
+
+
+    /**
+     * Creates a SQL condition for ordering by a JSON column with custom collation.
+     *
+     * @param string|FieldLayoutProviderInterface $provider
+     * @param string $fieldHandle
+     * @param string $collation
+     * @param string|null $key
+     * @return string
+     */
+    public static function orderByJsonColumn(string|FieldLayoutProviderInterface $provider, string $fieldHandle, string $collation = 'ci', ?string $key = null): string
     {
-        $entryType = Craft::$app->entries->getEntryTypeByHandle($entryTypeHandle);
-        if (!$entryType) {
-            throw new EntryTypeNotFoundException("Entry type '{$entryTypeHandle}' not found");
+
+        return sprintf(
+            '(%s COLLATE %s)',
+            static::getFieldSql($provider, $fieldHandle, $key),
+            match ($collation) {
+                'pb' => 'utf8mb4_de_pb_0900_ai_ci',
+                'ci' => 'utf8mb4_0900_ai_ci',
+                'cs' => 'utf8mb4_0900_as_cs',
+                default => $collation,
+            }
+        );
+    }
+
+    /**
+     * Creates a SQL condition for comparing a JSON column in lower case.
+     *
+     * @param FieldLayoutProviderInterface|string $provider
+     * @param string $fieldHandle
+     * @param string $term
+     * @param string|null $key
+     * @return string
+     */
+    public static function equalsLower(FieldLayoutProviderInterface|string $provider, string $fieldHandle, string $term, ?string $key): string
+    {
+
+        return sprintf(
+            "LOWER(%s) = LOWER('%s')",
+            static::getFieldSql($provider, $fieldHandle, $key),
+            str_replace("'", "''", $term)
+        );
+    }
+
+    public static function getFieldSql(FieldLayoutProviderInterface|string $provider, string $fieldHandle, ?string $key): ?string
+    {
+        return static::getField($provider, $fieldHandle)->getValueSql($key);
+    }
+
+    public static function getField(FieldLayoutProviderInterface|string $provider, string $fieldHandle): \craft\base\FieldInterface
+    {
+        if (is_string($provider)) {
+            $provider = Craft::$app->getEntries()->getEntryTypeByHandle($provider);
+            if (!$provider) {
+                throw new InvalidArgumentException('Entry type not found: ' . $provider);
+            }
         }
 
-        $fieldLayout = $entryType->getFieldLayout();
-        $field = $fieldLayout->getFieldByHandle($fieldHandle);
-
-        if(!$field) {
-            throw new InvalidFieldException("Field '{$fieldHandle}' not found");
+        $field = $provider->getFieldLayout()->getFieldByHandle($fieldHandle);
+        if (!$field) {
+            throw new InvalidArgumentException('Field not found: ' . $fieldHandle);
         }
 
-        $sql = $field->getValueSql();
-        if (!$sql) {
-            throw new InvalidFieldException("Field '{$fieldHandle}' does not support JSON custom field queries");
-        }
-
-        return $sql;
-
+        return $field;
     }
 }
--- src/web/twig/ExtrasExtension.php
+++ src/web/twig/ExtrasExtension.php
@@ -2,6 +2,7 @@
 
 namespace wsydney76\extras\web\twig;
 
+use craft\base\FieldLayoutProviderInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 use wsydney76\extras\helpers\ExtrasHelper;
@@ -16,9 +17,37 @@ class ExtrasExtension extends AbstractExtension
         // Define custom Twig functions
         // (see https://twig.symfony.com/doc/3.x/advanced.html#functions)
         return [
-            new TwigFunction('valueSql', function(string $entryTypeHandle, string $fieldHandle) {
-                return ExtrasHelper::getValueSql($entryTypeHandle, $fieldHandle);
-            }),
+            new TwigFunction('orderByJsonColumn', [$this, 'orderByJsonColumn']),
+            new TwigFunction('equalsLower', [$this, 'equalsLower']),
         ];
     }
+
+    /**
+     * Creates a SQL condition for ordering by a JSON column with custom collation.
+     *
+     * @param string|FieldLayoutProviderInterface $provider
+     * @param string $fieldHandle
+     * @param string $collation
+     * @param string|null $key
+     * @return string
+     */
+    public function orderByJsonColumn(string|FieldLayoutProviderInterface $provider, string $fieldHandle, string $collation = 'ci', ?string $key = null): string
+    {
+        return ExtrasHelper::orderByJsonColumn($provider, $fieldHandle, $collation, $key);
+    }
+
+    /**
+     * Creates a SQL condition for comparing a JSON column in lower case.
+     *
+     * @param string|FieldLayoutProviderInterface $provider
+     * @param string $fieldHandle
+     * @param string $term
+     * @param string|null $key
+     * @return string
+     */
+    public function equalsLower(string|FieldLayoutProviderInterface $provider, string $fieldHandle, string $term = '', ?string $key = null): string
+    {
+        return ExtrasHelper::equalsLower($provider, $fieldHandle, $term, $key);
+    }
+
 }
