9175f119b4af8857592ecace75023bfaf6711cee
wsydn <wsydney@web.de>
2025-01-24 19:14:08 +0100

    Ajax search

---
+++ src/controllers/ViewController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace wsydney76\extras\controllers;
+
+use Craft;
+use craft\web\Controller;
+
+class ViewController extends Controller
+{
+    protected array|bool|int $allowAnonymous = true;
+
+    public function actionRenderTemplate()
+    {
+        $template = $this->request->getRequiredBodyParam('template');
+        $variables = $this->request->getBodyParam('variables', []);
+        $templateMode = $this->request->getBodyParam('templateMode', Craft::$app->getView()->getTemplateMode());
+
+        return $this->asSuccess('Templated rendered', [
+            'html' => Craft::$app->getView()->renderTemplate($template, $variables, $templateMode)
+        ]);
+    }
+}
