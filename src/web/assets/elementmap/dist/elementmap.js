f4fe627857bb7f85253af1431a4c573cbc346fcb
wsydn <wsydney@web.de>
2026-03-08 11:34:21 +0100

    Add error handling for createProvisionalDraft event in element editor

--- src/web/assets/elementmap/dist/elementmap.js
+++ src/web/assets/elementmap/dist/elementmap.js
@@ -17,8 +17,12 @@ $btn.on('click', function() {
 });
 
 setTimeout(() => {
-    Craft.cp.$primaryForm.data('elementEditor').on('createProvisionalDraft', function() {
-        elementMapElementId = Craft.cp.$primaryForm.data('elementEditor').settings.elementId;
-    });
+    // May fail if called outside of an element editor, but we don't want to throw an error in that case
+    try {
+        Craft.cp.$primaryForm.data('elementEditor').on('createProvisionalDraft', function() {
+            elementMapElementId = Craft.cp.$primaryForm.data('elementEditor').settings.elementId;
+        });
+    } catch (e) {
+    }
 }, 500)
 
